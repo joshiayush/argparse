6e2f23369d04a317bc385354daff420fab8a7654
joshiayush <ayush854032@gmail.com>
2022-11-19 20:40:14 +0530

    Add private function `formatUsage()` to format the usage string for the parser
    This `formatUsage()` is used when there is no explicit `usage` string is
    given by the user. This `formatUsage()` function generates a string
    containing all the command line options with their help messages that were
    included in the parser object.
    
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- Argparse/Argparse.java
+++ Argparse/Argparse.java
@@ -1,36 +1,103 @@
 package Argparse;
 
 import java.io.File;
+import java.util.ArrayList;
 
-class ActionsContainer {
-    ActionsContainer(String description, String prefixChars) {
-        /* All the actions need to be registered here. */
+enum ArgparseOptionType {
+    ARGPARSE_OPT_END,
+    ARGPARSE_OPT_GROUP,
+    ARGPARSE_OPT_BOOLEAN,
+    ARGPARSE_OPT_BIT,
+    ARGPARSE_OPT_INTEGER,
+    ARGPARSE_OPT_FLOAT,
+    ARGPARSE_OPT_STRING,
+}
+
+class ArgparseOption {
+    public String shortName;
+    public String longName;
+    public String value;
+    public String help;
+    public ArgparseOptionType optionType;
+
+    public ArgparseOption(String shortName, String longName, String value, ArgparseOptionType optionType, String help) {
+        this.shortName = shortName;
+        this.longName = longName;
+        this.value = value;
+        this.optionType = optionType;
+        this.help = help;
     }
 }
 
-class ArgumentParser extends ActionsContainer {
-    private String progName;
-    private String progUsage;
-    private boolean addHelp;
+class Argparse {
+    private String progname;
+    private String usage;
 
-    public ArgumentParser(String[] sysArgs, String progName, String progUsage, String description,
-            String prefixChars, boolean addHelp) {
-        super(description, prefixChars);
-        if (progName == null || progName.isEmpty()) {
-            this.progName = getBaseName(sysArgs[0]);
-        }
-        if (progUsage == null) {
-            this.progUsage = "";
-        }
-        this.addHelp = addHelp;
+    /** A description of the command line software at the end of the usage. */
+    private String description;
 
-        char defaultPrefix = prefixChars.charAt(0);
-        if (this.addHelp) {
-            // Add help option here.
-        }
+    /** A description of the command line software at the end of the description. */
+    private String epilog;
+
+    private ArrayList<ArgparseOption> options;
+
+    Argparse(String[] sysargs, String progname, String usage, String description, String epilog) {
+        if (progname == null || progname.isEmpty())
+            this.progname = new File(sysargs[0]).getName();
+        this.usage = usage;
+        this.description = description;
+        this.epilog = epilog;
     }
 
-    private String getBaseName(String fileName) {
-        return new File(fileName).getName();
+    public void addArgument(final ArgparseOption argparseOption) {
+        options.add(argparseOption);
+    }
+
+    private String formatUsage() {
+        String formattedOptionsString = "";
+        for (final ArgparseOption option : this.options) {
+            if ((option.shortName == null || option.shortName.isEmpty())
+                    && (option.longName == null || option.longName.isEmpty()))
+                continue;
+            formattedOptionsString += "[";
+            if (option.shortName != null && !option.shortName.isEmpty())
+                formattedOptionsString += option.shortName + "|";
+            if (option.longName != null && !option.longName.isEmpty())
+                formattedOptionsString += option.longName;
+            formattedOptionsString += "]";
+        }
+        String formattedUsageString = "";
+        formattedUsageString += "Usage: " + this.progname + " " + formattedOptionsString + "\n";
+        formattedUsageString += "\n";
+        int indentWidth = 4;
+        for (final ArgparseOption option : options) {
+            if ((option.shortName == null || option.shortName.isEmpty())
+                    && (option.longName == null || option.longName.isEmpty()))
+                continue;
+            for (int i = 0; i < indentWidth; ++i)
+                formattedUsageString += " ";
+            if (option.shortName != null && !option.shortName.isEmpty())
+                formattedUsageString += option.shortName;
+            if (option.longName != null && !option.longName.isEmpty())
+                formattedUsageString += ", " + option.longName;
+            if (option.help != null && !option.help.isEmpty()) {
+                for (int i = 0; i < indentWidth; ++i)
+                    formattedUsageString += " ";
+                formattedUsageString += option.help;
+            }
+        }
+        return formattedUsageString;
+    }
+
+    public void printUsage() {
+        if (this.usage != null)
+            System.err.println(this.usage);
+        else
+            System.err.println(this.formatUsage());
+        System.err.println();
+        if (!this.description.isEmpty())
+            System.err.println(this.description);
+        if (!this.epilog.isEmpty())
+            System.err.println(this.epilog);
     }
 }
