ad4956615c1ef9529b4337915f21213915fdd91d
joshiayush <ayush854032@gmail.com>
2022-11-26 23:16:31 +0530

    Document enum `ArgparseOptionType`
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- src/main/java/Argparse/ArgparseOptionType.java
+++ src/main/java/Argparse/ArgparseOptionType.java
@@ -32,10 +32,46 @@
 package Argparse;
 
 public enum ArgparseOptionType {
+  /**
+   * This option does not have to do with values that an option can accept over
+   * the command-line but the way an option can be used over the command-line. A
+   * group restricts the use of options over the command-line. If there are
+   * several options that shares a group, only one at a time can be used over the
+   * command-line.
+   */
   ARGPARSE_OPT_GROUP,
+
+  /**
+   * These types of options can only accept boolean values or there presence over
+   * the command-line can also be considered as a boolean value for them, means if
+   * they are present over the command-line the value would be considered as true,
+   * false otherwise.
+   */
   ARGPARSE_OPT_BOOLEAN,
+
+  /**
+   * These types of options can only accept bit values. The only two discrete bit
+   * values that we can have is 0 and 1. None other than these values can be used
+   * over the command-line for these types of arguments or an error will be
+   * raised.
+   */
   ARGPARSE_OPT_BIT,
+
+  /**
+   * These types of options can only accept integer values i.e., can only accept
+   * those strings that can be parsed to a valid integer value.
+   */
   ARGPARSE_OPT_INTEGER,
+
+  /**
+   * These types of options can only accept float values i.e., can only accept
+   * those strings that can be parsed to a valid floating point value.
+   */
   ARGPARSE_OPT_FLOAT,
+
+  /**
+   * These types of options can only accept strings values. By default everything
+   * given over the command line is a string value.
+   */
   ARGPARSE_OPT_STRING,
 }
